3f3f04b7a993cd31c4193cc8e5516d9957d7d018
Jackline-ke <antoninahodhiambo@gmail.com>
2023-06-07 17:22:46 +0300

    Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,14 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get "/movies" do
+    movies = Movies.all
+    movies.to_json
+  end
+
+  get "/categories" do
+    categories = Categories.all
+    categories.to_json
+  end
+
 end
